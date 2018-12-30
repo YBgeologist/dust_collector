740cf2ccd88c58a762c00e9e71eebbb9ec46d5bc
Yuval Burstyn <42605542+YBgeologist@users.noreply.github.com>
2018-12-30 16:16:47 +0200

    test of switch side
    A. Switch always on at 1023
    B. <1000 means switch off/clicked
    The code here will tell you what analog switch means which side and the reading to expect when switch is "off"

---
+++ Switch_side.ino
@@ -0,0 +1,22 @@
+// Test Engine
+
+// Switches are always ON at 1023, when hat marks switch that means under 1000
+
+//const int analogDry =  1;// on wet switch
+const int analogDry =  0;// on dry switch
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+    Serial.begin(9600);
+     
+}
+
+// the loop function runs over and over again forever
+void loop() {
+    
+  int sensorValue = analogRead(analogDry);
+  Serial.println(sensorValue);
+ 
+}
+
