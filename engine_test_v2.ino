c811ded18fa41e0b6ba2654c6968aaa7bdba8513
Yuval Burstyn <42605542+YBgeologist@users.noreply.github.com>
2018-12-30 16:03:04 +0200

    Engine test 2
    this will tell you correct pins and correct direction / stop

---
+++ engine_test_v2.ino
@@ -0,0 +1,31 @@
+const int forwards = 4; // forward is wet
+const int backwards = 5;//assign relay INx pin to arduino pin
+
+void setup() {
+ 
+pinMode(forwards, OUTPUT);//set relay as an output
+pinMode(backwards, OUTPUT);//set relay as an output
+
+}
+
+void loop() {
+
+digitalWrite(forwards, HIGH);
+digitalWrite(backwards, HIGH);//Deactivate both relays to brake the motor
+//delay(10000);// wait 10 seconds
+/*
+ digitalWrite(forwards, HIGH);
+ digitalWrite(backwards, LOW);//Activate the relay the dryther direction, they must be different to move the motor
+ delay(10000);// wait 10 seconds
+
+ digitalWrite(forwards, LOW);
+ digitalWrite(backwards, HIGH);//Activate the relay wet direction, they must be different to move the motor
+ delay(100000); // wait 10 seconds
+
+ digitalWrite(forwards, HIGH);
+ digitalWrite(backwards, HIGH);//Deactivate both relays to brake the motor
+ delay(10000);// wait 10 seconds
+*/
+}
+
+ 
